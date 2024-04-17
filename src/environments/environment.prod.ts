4a281b66f2ce3e74da10de8a5ccc38cef415cb8c
Canh Hung <canhhung.nguyen@vn.panasonic.com>
2024-04-17 11:11:03 +0700

    coding

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://10.92.177.230:8889/pidvn-be',
+  baseUrl: 'http://10.92.176.57:8889/pidvn-be',
   baseUrlPhp: 'https://10.92.176.57:8080',
   weatherbit:'https://api.weatherbit.io/v2.0/current?key=ec87ff6a58bf4f92969b0fd823bdc686',
   baseUrlJava: 'http://10.92.176.57:6969',
