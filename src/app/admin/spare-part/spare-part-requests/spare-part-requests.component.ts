54fadde87aeacb026d35fa8dc9c3732e799537c5
Canh Hung <canhhung.nguyen@vn.panasonic.com>
2024-07-04 17:13:21 +0700

    sua lai phan request spare part

--- src/app/admin/spare-part/spare-part-requests/spare-part-requests.component.ts
+++ src/app/admin/spare-part/spare-part-requests/spare-part-requests.component.ts
@@ -11,11 +11,9 @@ import { SparePartRequestsService } from './spare-part-requests.service';
   styleUrls: ['./spare-part-requests.component.scss'],
 })
 export class SparePartRequestsComponent implements OnInit {
-
-
   @ViewChild(DxValidatorComponent, { static: false })
   validator!: DxValidatorComponent;
-  
+
   @ViewChild(DxDataGridComponent, { static: false })
   orderedGrid!: DxDataGridComponent;
 
@@ -26,23 +24,21 @@ export class SparePartRequestsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getRequests()
+    this.getRequests();
     this.getSpareParts();
     this.getSubsections();
   }
 
-  
   requests: any;
   isOpenRequestModal: boolean = false;
-  isOpenRequestDetailModal: boolean = false
+  isOpenRequestDetailModal: boolean = false;
   spareParts!: SparePartRecordVo[];
-  sparePartSelectedList: any;  // Danh sách các mã được chọn
 
   subsectionSelected: any; // Bộ phận được chọn
-  subsections:any; // Danh sách bộ phận yêu cầu hàng
+  subsections: any; // Danh sách bộ phận yêu cầu hàng
 
-  factorySelected:any
-  factories:any = [
+  factorySelected: any;
+  factories: any = [
     { code: 'RE', name: 'RELAY FACTORY' },
     { code: 'EM', name: 'EMC FACTORY' },
     { code: 'PN', name: 'PIH ENC FACTORY' },
@@ -56,12 +52,15 @@ export class SparePartRequestsComponent implements OnInit {
   sparePartsReqDetail: any; // danh sách hàng theo request
   sparePartReqSelected: any;
 
+
+  // 2 biến này để hứng dữ liệu order list
+  orderListSet: any = new Set();
+  orderListArr: any;
+
   getRequests() {
-    this.sparePartRequestSvc.getRequests().subscribe(
-      response => {
-        this.requests = response
-      }
-    )
+    this.sparePartRequestSvc.getRequests().subscribe((response) => {
+      this.requests = response;
+    });
   }
 
   openRequestModal() {
@@ -78,42 +77,9 @@ export class SparePartRequestsComponent implements OnInit {
    * Lấy ra các khu vực
    */
   getSubsections() {
-    this.sparePartRequestSvc.getSubsections().subscribe(
-      response => {
-        this.subsections = response
-      }
-    )
-  }
-
-  /**
-   * Sự kiện khi click vào từng row
-   * @param event
-   */
-  onSelectionChanged(event: any) {
-
-  
-    // Check chọn khu vực
-    if (!this.factorySelected) {
-      this.toastr.warning('Cần chọn khu vực','Warning')
-      return
-    }
-    // Check chọn bộ phận
-    if (!this.subsectionSelected) {
-      this.toastr.warning('Cần chọn bộ phận','Warning')
-      return
-    }
-   
-    let arr = new Array();
-
-    event.selectedRowsData.forEach((item: any) => {
-      let obj = {...item, qty:0, factoryCode: this.factorySelected, subsectionId: this.subsectionSelected }
-      arr.push(obj)
+    this.sparePartRequestSvc.getSubsections().subscribe((response) => {
+      this.subsections = response;
     });
-
-    this.sparePartSelectedList = arr
-
-    console.log('this.sparePartSelected: ', this.sparePartSelectedList);
-    
   }
 
   async onSaving(event: any) {
@@ -122,87 +88,113 @@ export class SparePartRequestsComponent implements OnInit {
     await event.changes.forEach((item: any) => {
       arr.push(item.key);
     });
-    
 
     /**
      * Xử lý lưu list data
      */
 
     console.log('onSaving: ', arr);
-    
 
-    this.sparePartRequestSvc.createRequest(arr, this.factorySelected, this.subsectionSelected).subscribe(
-      response => {
-        this.getRequests()
-      }
-    )
-
+    this.sparePartRequestSvc
+      .createRequest(arr, this.factorySelected, this.subsectionSelected)
+      .subscribe((response) => {
+        this.getRequests();
+      });
 
     this.closeRequestModal();
-    
-
-    
-
   }
 
   onCreateRequest(dataGrid: DxDataGridComponent) {
-    console.log('onCreateRequest');
-    
     dataGrid.instance.saveEditData();
   }
-
 
   /**
    * Đóng Request Modal
    */
   closeRequestModal() {
     this.isOpenRequestModal = false;
-    this.sparePartSelectedList = null;
+    //this.sparePartSelectedList = null;
+    this.orderListArr = new Array();
+    this.orderListSet = new Set();
   }
 
-
   openRequestDetailModal(item: any) {
-    
-    this.sparePartReqSelected = item
-    
+    this.sparePartReqSelected = item;
     console.log(this.sparePartReqSelected);
-    
-    this.sparePartRequestSvc.getRequestDetail(item.data.id).subscribe(
-      response => {
-        this.sparePartsReqDetail = response
-        this.isOpenRequestDetailModal = true
-      }
-    )
+    this.sparePartRequestSvc
+      .getRequestDetail(item.data.id)
+      .subscribe((response) => {
+        this.sparePartsReqDetail = response;
+        this.isOpenRequestDetailModal = true;
+      });
   }
 
   downloadRequestDetail(event: any) {
-    
-    this.sparePartRequestSvc.downloadM4M8Request(event.data.id).subscribe(response => {
+    this.sparePartRequestSvc
+      .downloadM4M8Request(event.data.id)
+      .subscribe((response) => {
+        const blob = new Blob([response], {
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        });
 
-      const blob = new Blob([response], {
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        const data = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = data;
+        link.download = `RequestDetail.xlsm`;
+        link.dispatchEvent(
+          new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window,
+          })
+        );
+
+        setTimeout(() => {
+          window.URL.revokeObjectURL(data);
+          link.remove();
+        }, 100);
       });
+  }
 
-      const data = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = data;
-      link.download = `RequestDetail.xlsm`;
-      link.dispatchEvent(
-        new MouseEvent('click', {
-          bubbles: true,
-          cancelable: true,
-          view: window,
-        })
-      );
+  /**
+   * Thêm dữ liệu vào request
+   * @param item
+   */
+  addToRequest(item: any) {
+    // Check chọn khu vực
+    if (!this.factorySelected) {
+      this.toastr.warning('Cần chọn khu vực', 'Warning');
+      return;
+    }
+    // Check chọn bộ phận
+    if (!this.subsectionSelected) {
+      this.toastr.warning('Cần chọn bộ phận', 'Warning');
+      return;
+    }
 
-      setTimeout(() => {
-        window.URL.revokeObjectURL(data);
-        link.remove();
-      }, 100);
-    });
+    let obj = {
+      ...item.data,
+      qty: 0,
+      factoryCode: this.factorySelected,
+      subsectionId: this.subsectionSelected,
+    };
 
+    this.orderListSet.add(obj);
 
+    this.orderListArr = Array.from(this.orderListSet);
+  }
 
-
+  /**
+   * Xóa bớt các item đã order
+   * @param item 
+   */
+  deleteItemOrderList(item: any) {
+    for (let obj of this.orderListSet) {
+      if (obj.partNumber === item.data.partNumber) {
+        this.orderListSet.delete(obj);
+        break;
+      }
+    }
+    this.orderListArr = Array.from(this.orderListSet);
   }
 }
