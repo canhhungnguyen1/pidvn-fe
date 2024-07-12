14e8bc0e14645794c8c1b465dc55a67b2295113e
Canh Hung <canhhung.nguyen@vn.panasonic.com>
2024-07-12 17:15:30 +0700

    coding

--- src/app/admin/ie/drawing-control/ie-dc-project-detail/ie-dc-project-detail.component.ts
+++ src/app/admin/ie/drawing-control/ie-dc-project-detail/ie-dc-project-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-ie-dc-project-detail',
   templateUrl: './ie-dc-project-detail.component.html',
@@ -206,4 +207,130 @@ export class IeDcProjectDetailComponent implements OnInit {
 
   isOpenActivityModal: boolean = false;
   
+
+
+
+
+
+
+
+  onSaveClick(data: any) {
+    // Xử lý lưu dữ liệu
+    alert('Save button clicked');
+    // Thêm logic để lưu dữ liệu tại đây
+  }
+
+  onCancelClick() {
+    // Xử lý hủy bỏ
+    alert('Cancel button clicked');
+    // Thêm logic để hủy bỏ tại đây
+  }
+
+
+employees = [
+  {
+    ID: 1,
+    Head_ID: -1,
+    Full_Name: 'John Heart',
+    Prefix: 'Mr.',
+    Title: 'CEO',
+    City: 'Los Angeles',
+    State: 'California',
+    Email: 'jheart@dx-email.com',
+    Skype: 'jheart_DX_skype',
+    Mobile_Phone: '(213) 555-9392',
+    Birth_Date: '1964-03-16',
+    Hire_Date: '1995-01-15',
+  }, {
+    ID: 2,
+    Head_ID: 1,
+    Full_Name: 'Samantha Bright',
+    Prefix: 'Dr.',
+    Title: 'COO',
+    City: 'Los Angeles',
+    State: 'California',
+    Email: 'samanthab@dx-email.com',
+    Skype: 'samanthab_DX_skype',
+    Mobile_Phone: '(213) 555-2858',
+    Birth_Date: '1966-05-02',
+    Hire_Date: '2004-05-24',
+  }, {
+    ID: 3,
+    Head_ID: 1,
+    Full_Name: 'Arthur Miller',
+    Prefix: 'Mr.',
+    Title: 'CTO',
+    City: 'Denver',
+    State: 'Colorado',
+    Email: 'arthurm@dx-email.com',
+    Skype: 'arthurm_DX_skype',
+    Mobile_Phone: '(310) 555-8583',
+    Birth_Date: '1972-07-11',
+    Hire_Date: '2007-12-18',
+  }, {
+    ID: 4,
+    Head_ID: 1,
+    Full_Name: 'Robert Reagan',
+    Prefix: 'Mr.',
+    Title: 'CMO',
+    City: 'Bentonville',
+    State: 'Arkansas',
+    Email: 'robertr@dx-email.com',
+    Skype: 'robertr_DX_skype',
+    Mobile_Phone: '(818) 555-2387',
+    Birth_Date: '1974-09-07',
+    Hire_Date: '2002-11-08',
+  }, {
+    ID: 5,
+    Head_ID: 1,
+    Full_Name: 'Greta Sims',
+    Prefix: 'Ms.',
+    Title: 'HR Manager',
+    City: 'Atlanta',
+    State: 'Georgia',
+    Email: 'gretas@dx-email.com',
+    Skype: 'gretas_DX_skype',
+    Mobile_Phone: '(818) 555-6546',
+    Birth_Date: '1977-11-22',
+    Hire_Date: '1998-04-23',
+  }, {
+    ID: 6,
+    Head_ID: 3,
+    Full_Name: 'Brett Wade',
+    Prefix: 'Mr.',
+    Title: 'IT Manager',
+    City: 'Reno',
+    State: 'Nevada',
+    Email: 'brettw@dx-email.com',
+    Skype: 'brettw_DX_skype',
+    Mobile_Phone: '(626) 555-0358',
+    Birth_Date: '1968-12-01',
+    Hire_Date: '2009-03-06',
+  }, {
+    ID: 7,
+    Head_ID: 5,
+    Full_Name: 'Sandra Johnson',
+    Prefix: 'Mrs.',
+    Title: 'Controller',
+    City: 'Beaver',
+    State: 'Utah',
+    Email: 'sandraj@dx-email.com',
+    Skype: 'sandraj_DX_skype',
+    Mobile_Phone: '(562) 555-2082',
+    Birth_Date: '1974-11-15',
+    Hire_Date: '2005-05-11',
+  }, {
+    ID: 8,
+    Head_ID: 4,
+    Full_Name: 'Ed Holmes',
+    Prefix: 'Dr.',
+    Title: 'Sales Manager',
+    City: 'Malibu',
+    State: 'California',
+    Email: 'edwardh@dx-email.com',
+    Skype: 'edwardh_DX_skype',
+    Mobile_Phone: '(310) 555-1288',
+    Birth_Date: '1973-07-14',
+    Hire_Date: '2005-06-19',
+  }]
 }
