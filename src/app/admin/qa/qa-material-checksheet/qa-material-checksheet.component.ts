39dee30cea51a50fe02b8e454dd85634bca4a40b
Canh Hung <canhhung.nguyen@vn.panasonic.com>
2025-03-11 14:24:00 +0700

    sua checksheet qa

--- src/app/admin/qa/qa-material-checksheet/qa-material-checksheet.component.ts
+++ src/app/admin/qa/qa-material-checksheet/qa-material-checksheet.component.ts
@@ -13,7 +13,6 @@ import { QaMaterialCheckSheetService } from './qa-material-checksheet.service';
 export class QaMaterialChecksheetComponent implements OnInit {
   @ViewChild('labelIpt') labelIpt!: DxTextBoxComponent;
   @ViewChild('userIdIpt') userIdIpt!: DxTextBoxComponent;
-  
 
   @ViewChild(DxDataGridComponent, { static: false })
   psMasterGrid!: DxDataGridComponent;
@@ -74,7 +73,6 @@ export class QaMaterialChecksheetComponent implements OnInit {
     this.qaCardSelected = event;
     this.isOpenPsMasterModal = true;
 
-
     setTimeout(() => {
       this.userIdIpt.instance.focus();
     }, 500);
@@ -93,10 +91,7 @@ export class QaMaterialChecksheetComponent implements OnInit {
       .subscribe((response) => {
         this.psMasters = response.result;
         this.psMasterArr = this.convertPsMasterToArray(this.psMasters);
-        
       });
-
-    
   }
 
   convertPsMasterToArray(psMasters: any) {
@@ -148,24 +143,24 @@ export class QaMaterialChecksheetComponent implements OnInit {
       qaCard: this.qaCardSelected.qaCard,
       lotNo: lotNo,
       label: label,
-      username: this.userScan
+      username: this.userScan,
     };
 
     if (!this.psMasterArr.includes(partNo)) {
       this.toastr.error('SAI NVL');
       obj.remark = 'SAI NVL';
+      obj.result = 'NG';
+    } else {
+      obj.remark = 'OK';
+      obj.result = 'NG';
     }
 
-    this.qaMaterialCheckSheetSvc.scanMaterial(obj).subscribe(
-      response => {
-        this.checkSheetRecords = response.result.data
-      }
-    )
-
+    this.qaMaterialCheckSheetSvc.scanMaterial(obj).subscribe((response) => {
+      this.checkSheetRecords = response.result.data;
+    });
 
     this.selectTextInput('labelIpt');
   }
-
 
   scanUserId(event: any) {
     let dataScan = event.target.value;
@@ -173,16 +168,14 @@ export class QaMaterialChecksheetComponent implements OnInit {
     // Nếu chưa scan mã nhân viên thì thông báo
 
     if (isNaN(dataScan)) {
-      this.toastr.warning('Cần scan mã nhân viên','Warning')
-      this.selectTextInput('userIdIpt')
-      return
+      this.toastr.warning('Cần scan mã nhân viên', 'Warning');
+      this.selectTextInput('userIdIpt');
+      return;
     }
 
     this.userScan = dataScan;
 
     this.labelIpt.instance.focus();
-
-
   }
 
   selectTextInput(input: string) {
@@ -208,12 +201,25 @@ export class QaMaterialChecksheetComponent implements OnInit {
   }
 
   onCellPrepared(e: any) {
-    if (e.rowType === 'data' && e.column.dataField === 'remark') {
-      const remark = e.row.data.remark;
-      if (remark === 'SAI NVL') {
-        e.cellElement.style.color = 'red'; // Màu chữ đỏ
+    if (e.rowType === 'data') {
+      const result = e.row.data.result;
+
+      if (result === 'NG') {
+        e.cellElement.style.backgroundColor = 'red'; // Change background color
+        e.cellElement.style.color = 'white'; // Change text color for better visibility
       }
     }
   }
 
+  onRowClickQaCard(e: any) {
+    console.log('onRowClickQaCard: ', e);
+    this.openPsMasterDetailModal(e.data);
+  }
+
+  onCellPreparedQaCard(e: any) {
+    if (e.rowType === 'header') {
+      e.cellElement.style.backgroundColor = '#000080'; // Change background color
+      e.cellElement.style.color = '#ffffff'; // Change text color for better visibility
+    }
+  }
 }
