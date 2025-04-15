3c51495c476f0b85b569c4bdc12e035f85c4e36c
Canh Hung <canhhung.nguyen@vn.panasonic.com>
2025-04-15 08:57:52 +0700

    upload file ie anh Quan

--- src/app/admin/ie/ie.module.ts
+++ src/app/admin/ie/ie.module.ts
@@ -1,24 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import {
+  DxButtonModule,
+  DxDataGridModule,
+  DxFileUploaderModule,
+  DxTextBoxModule,
+} from 'devextreme-angular';
 import { DxTabsModule } from 'devextreme-angular/ui/tabs';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { IeRoutingModule } from './ie-routing.module';
 import { IeComponent } from './ie.component';
 import { IeMaDoComponent } from './machine-document/ie-ma-do.component';
-import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import {
-  DxTextBoxModule,
-  DxFileUploaderModule,
-  DxButtonModule,
-} from 'devextreme-angular';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 @NgModule({
   declarations: [IeComponent, IeMaDoComponent],
   imports: [
     CommonModule,
+    FormsModule,
     IeRoutingModule,
     DxTabsModule,
     NzTabsModule,
@@ -28,7 +35,12 @@ import { NzInputModule } from 'ng-zorro-antd/input';
     NzTableModule,
     NzModalModule,
     NzButtonModule,
-    NzInputModule
+    NzInputModule,
+    NzSelectModule,
+    NzCollapseModule,
+    NzGridModule,
+    DxDataGridModule,
+    NzIconModule
   ],
 })
 export class IeModule {}
--- src/app/admin/ie/machine-document/ie-ma-do.component.ts
+++ src/app/admin/ie/machine-document/ie-ma-do.component.ts
@@ -1,19 +1,94 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DxDataGridComponent, DxFileUploaderComponent } from 'devextreme-angular';
+import { IeMaDoService } from './services/ie-ma-do.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Component({
   selector: 'app-ie-ma-do',
   templateUrl: './ie-ma-do.component.html',
-  styleUrl: './ie-ma-do.component.scss'
+  styleUrl: './ie-ma-do.component.scss',
 })
-export class IeMaDoComponent {
+export class IeMaDoComponent implements OnInit {
+
+  @ViewChild(DxDataGridComponent, { static: false })
+  machineFileGrid!: DxDataGridComponent;
+
+  @ViewChild('fileUploader', { static: false })
+  fileUploader!: DxFileUploaderComponent;
+
   
 
+  constructor(
+    private ieMaDoSvc: IeMaDoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private jwtHelperSvc: JwtHelperService
+  ) {}
 
-  isOpenUploadModal: boolean = false
+  ngOnInit(): void {
+    this.getMachines();
+    this.getMachineFiles();
+  }
 
+  isOpenUploadModal: boolean = false;
+  machineCode: string | null = null;
+  machines: any;
+  machineFiles: any;
+  isLoading: boolean = false
+
+  getMachineFiles() {
+    this.machineFileGrid?.instance.beginCustomLoading(
+      `Đang tải dữ liệu ...`
+    );
+
+    this.ieMaDoSvc.getMachinesFiles().subscribe((response) => {
+      this.machineFiles = response.result;
+      this.machineFileGrid.instance.endCustomLoading();
+    });
+  }
+
+  getMachines() {
+    this.ieMaDoSvc.getMachines().subscribe((response) => {
+      this.machines = response.result;
+    });
+  }
 
   uploadFile() {
-    
+
+    if (this.machineCode == null) {
+      this.toastr.warning("Máy không được để trống","Warning")
+      return
+    }
+
+    this.isLoading = true;
+    const selectedFile = this.fileUploader;
+
+    console.log('uploadFile: ', selectedFile.value[0]);
+
+    let createdBy = this.jwtHelperSvc.decodeToken(
+      localStorage.getItem('accessToken')?.toString()
+    ).Username;
+
+    let obj = {
+      machineCode: this.machineCode,
+      createdBy: createdBy,
+    };
+
+    this.ieMaDoSvc.uploadFile(selectedFile.value, obj).subscribe((response) => {
+      console.log('response: ', response);
+      this.getMachineFiles();
+      this.isLoading = false;
+      
+      this.clearFiles();
+    });
+  }
+
+  clearFiles() {
+    this.fileUploader.instance.reset();
+    this.machineCode = null;
+    // this.getMachines();
   }
 }
