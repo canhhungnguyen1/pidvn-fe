cdaa1e915a5bd3cebd509dbbf28548720e05c4aa
canhhung.nguyen1 <canhhung.nguyen1@gmail.com>
2025-05-11 15:17:39 +0700

    coding

--- src/app/admin/is/is-device-mng/is-device-mng-routing.module.ts
+++ src/app/admin/is/is-device-mng/is-device-mng-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IsDvMngMenuComponent } from './is-dv-mng-menu/is-dv-mng-menu.component';
 import { IsDvMngDevicesComponent } from './is-dv-mng-devices/is-dv-mng-devices.component';
-import { IsDvMngHistoryComponent } from './is-dv-mng-history/is-dv-mng-history.component';
+import { IsDvMngMenuComponent } from './is-dv-mng-menu/is-dv-mng-menu.component';
 
 const routes: Routes = [
   {
@@ -12,10 +11,6 @@ const routes: Routes = [
   {
     path: 'devices',
     component: IsDvMngDevicesComponent
-  },
-  {
-    path: 'history',
-    component: IsDvMngHistoryComponent
   }
 ];
 
--- src/app/admin/is/is-device-mng/is-device-mng.module.ts
+++ src/app/admin/is/is-device-mng/is-device-mng.module.ts
@@ -7,18 +7,17 @@ import {
   DxSelectBoxModule,
 } from 'devextreme-angular';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { IsDeviceMngRoutingModule } from './is-device-mng-routing.module';
 import { IsDvMngDevicesComponent } from './is-dv-mng-devices/is-dv-mng-devices.component';
-import { IsDvMngHistoryComponent } from './is-dv-mng-history/is-dv-mng-history.component';
 import { IsDvMngMenuComponent } from './is-dv-mng-menu/is-dv-mng-menu.component';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzImageModule } from 'ng-zorro-antd/image';
 @NgModule({
-  declarations: [IsDvMngMenuComponent, IsDvMngDevicesComponent, IsDvMngHistoryComponent],
+  declarations: [IsDvMngMenuComponent, IsDvMngDevicesComponent],
   imports: [
     CommonModule,
     IsDeviceMngRoutingModule,
