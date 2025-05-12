e4c7aa2165417a29b256950a8364d587c6f03513
Canh Hung <canhhung.nguyen@vn.panasonic.com>
2025-05-12 12:39:18 +0700

    create component inventory

---
+++ src/app/admin/is/is-device-mng/is-dv-mng-inventory/is-dv-mng-ivt-req-detail/is-dv-mng-ivt-req-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-is-dv-mng-ivt-req-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './is-dv-mng-ivt-req-detail.component.html',
+  styleUrl: './is-dv-mng-ivt-req-detail.component.scss'
+})
+export class IsDvMngIvtReqDetailComponent {
+
+}
---
+++ src/app/admin/is/is-device-mng/is-dv-mng-inventory/is-dv-mng-ivt-req/is-dv-mng-ivt-req.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-is-dv-mng-ivt-req',
+  standalone: true,
+  imports: [],
+  templateUrl: './is-dv-mng-ivt-req.component.html',
+  styleUrl: './is-dv-mng-ivt-req.component.scss'
+})
+export class IsDvMngIvtReqComponent {
+
+}
